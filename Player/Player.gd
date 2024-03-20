5f34005c87b4268c65d10633fb01e4322f1ee80f
Quilled <alytarfa@gmail.com>
2024-03-20 18:07:55 -0400

    change: smoother car audio

--- Player/Player.gd
+++ Player/Player.gd
@@ -13,4 +13,4 @@ func _physics_process(delta):
 	$RR.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
 	rpm = abs($RL.get_rpm())
 	$RL.engine_force = acceleration * max_torque * (1 - rpm / max_rpm)
-	audio.pitch_scale = (rpm / max_rpm) + 0.2
+	audio.pitch_scale = lerp(audio.pitch_scale, (rpm / max_rpm) + 0.2, 0.2)
